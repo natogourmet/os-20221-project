93bf8fb6d1f9cbb873286f8826dc6aa6608f4d9e
natogourmet <natogourmet@hotmail.com>
2022-09-05 17:25:13 -0500

    reading stat from all pids

--- dirContent.c
+++ dirContent.c
@@ -1,6 +1,8 @@
 
-#include<stdio.h>
-#include<dirent.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <ctype.h>
 
 int main(void)
 {
@@ -11,10 +13,35 @@ int main(void)
     {
         while ((dir = readdir(d)) != NULL)
         {
+            char filename[1000];
+
+            int isNumber = 1;
+            char *pid = dir->d_name;
+            for (int i = 0; i < strlen(pid); i++)
+            {
+                if (isdigit(pid[i]))
+                    printf("The string contains int\n");
+                else
+                    isNumber = 0;
+            }
+            if (isNumber == 0) continue;
+
+            
+            sprintf(filename, "/proc/%s/stat", dir->d_name);
+            FILE *f = fopen(filename, "r");
+
+            int unused;
+            char comm[1000];
+            char state;
+            int ppid;
+            fscanf(f, "%d %s %c %d", &unused, comm, &state, &ppid);
+            printf("comm = %s\n", comm);
+            printf("state = %c\n", state);
+            printf("parent pid = %d\n", ppid);
+            fclose(f);
             printf("%s\n", dir->d_name);
         }
         closedir(d);
     }
-    return(0);
+    return (0);
 }
-
